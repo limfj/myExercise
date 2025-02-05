de6585edd426f56bac5975fe72d71f80c5fe1c0a
limfj <39509427+limfj@users.noreply.github.com>
2025-02-05 16:35:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ df['Question'] = df['Question'].astype(str).str.lower().str.replace('[^\w\s]', '
 # Initialize OpenAI API key
 
 # Function to get embedding
-def get_embedding(text, model="text-embedding-ada-002"):
-   return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
+# def get_embedding(text, model="text-embedding-ada-002"):
+ #  return openai.Embedding.create(input = [text], model=model)['data'][0]['embedding']
 
 # Pre-calculate embeddings for all questions in the dataset
-df['Question_Embedding'] = df['Question'].apply(get_embedding)
+#df['Question_Embedding'] = df['Question'].apply(get_embedding)
 
 # Save the DataFrame with embeddings and similarities to a CSV file
-df.to_csv("qa_dataset_with_embeddings.csv", index=False)  # index=False to avoid saving row numbers
+#df.to_csv("qa_dataset_with_embeddings.csv", index=False)  # index=False to avoid saving row numbers
